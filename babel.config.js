e686a842c5db13842e43da321b39272b48d015a5
jwbth <jack.who.built.the.house@outlook.com>
2025-08-22 22:18:04 +0400

    .
    Change-Id: Ibc7e5e886857354e666cd9ef2f7320f6a8d8e6c2

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+// babel.config.js
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      bugfixes: true
+    }]
+  ],
+  plugins: [
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-class-static-block',
+    '@babel/plugin-transform-logical-assignment-operators',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+    '@babel/plugin-transform-optional-catch-binding',
+    '@babel/plugin-transform-optional-chaining',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-typescript',
+  ]
+};
