984d4b5fb561409c3c9d015210488aff38d05855
jwbth <jack.who.built.the.house@outlook.com>
2025-09-17 11:40:07 +0400

    Fix webpack-dev-server
    Change-Id: I55456cd42959c618729cea0aaf4af09391c549af

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -227,7 +227,16 @@ const webpack_ = (env) => {
         directory: path.join(__dirname, 'dist'),
       },
       port: 9000,
+      client: {
+        webSocketURL: 'ws://localhost:9000/ws',
+      },
       liveReload: false,
+
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      },
 
       // Fixes "GET https://localhost:9000/sockjs-node/info?t=... net::ERR_SSL_PROTOCOL_ERROR".
       allowedHosts: 'all',
